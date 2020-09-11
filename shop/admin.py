76757fb979ee814e1d6d3f7a88d6a4b1685fb77a
yarik2215 <stikblacklabel@gmail.com>
2020-09-12 01:43:46 +0300

    add customer model

--- shop/admin.py
+++ shop/admin.py
@@ -1,14 +1,39 @@
 from django.contrib import admin
-from .models import Item, ItemType, Order
+from .models import Item, ItemType, Order, OrderItems, Customer
+
+
+admin.site.register(ItemType)
+
 
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
-    list_display = ('slug','name','item_type','color_name')
+    list_display = ('slug','name','item_type','color_name','price')
     list_filter = ('item_type',)
     search_fields = ('name','color_name')
     prepopulated_fields = {'slug': ('name','color_name')}
-    
 
-admin.site.register(ItemType)
-admin.site.register(Order)
+
+class OrderItemsInline(admin.StackedInline):
+    model = OrderItems
+    extra = 1
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'get_user_name', 'order_date')
+    inlines = (OrderItemsInline,)
+    date_hierarchy = 'order_date'
+
+    def get_user_name(self, obj):
+        return obj.customer.username
+
+    def get_user_address(self, obj):
+        return obj.customer.address
+
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'phone', 'last_login')
+    search_fields = ('email', 'first_name', 'last_name', 'phone')
 
--- shop/models.py
+++ shop/models.py
@@ -1,39 +1,75 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class ItemType(models.Model):
+    '''
+    Item type model, store type of items that can be in shop.
+    '''
     name = models.CharField(verbose_name="name", max_length=100)
 
     def __str__(self):
         return self.name
 
+
+
 def upload_path(instance, filename):
-        return f"uploads/{instance.slug}.png"
-# upload_path = 'uploads/'
+    '''
+    Function that create path for saving item img depends on item.slug field.
+    '''
+    return f"uploads/{instance.slug}.png"
 
 class Item(models.Model):
+    '''
+    Shop item model.
+    '''
     name = models.CharField(verbose_name="name", max_length=100)
     color_name = models.CharField(verbose_name="color name", max_length=50)
     slug = models.SlugField(max_length=200, unique=True)
     item_type = models.ForeignKey(ItemType, on_delete=models.SET_NULL, null=True, blank=True)
     image = models.ImageField(upload_to=upload_path)
     quantity = models.IntegerField()
+    price = models.DecimalField("price",max_digits=10,decimal_places=2)
 
     def __str__(self):
         return self.name
     
 
-class ShopUser(models.Model):
-    first_name = models.CharField(verbose_name="first name", max_length=100)
-    last_name = models.CharField(verbose_name="last name", max_length=100)
-    phone = models.CharField(verbose_name="phone number", max_length=20) #TODO: add phone validator validators=[]
-    email = models.EmailField(max_length=100)
 
-    def __str__(self):
-        return f"{self.first_name} {self.last_name}"
-    
 
+class OrderItems(models.Model):
+    '''
+    M2M relation table between Order and Item 
+    '''
+    order = models.ForeignKey('Order',on_delete=models.CASCADE)
+    item = models.ForeignKey('Item',on_delete=models.CASCADE)
+    item_count = models.IntegerField()
+
+
+
+#TODO: добавить возможность незалогиненым юзерам совершать покупки
 class Order(models.Model):
-    order_date = models.DateField(verbose_name="order date")
-    user_id = models.ForeignKey(ShopUser, on_delete=models.CASCADE)
+    '''
+    Order model, store information about orders.
+    '''
+    customer = models.ForeignKey('Customer', on_delete=models.CASCADE)  # add validation that user is in customer group
+    items = models.ManyToManyField('Item',through='OrderItems')
+    order_date = models.DateField("order date")
+    delivery_address = models.CharField(max_length=255)
+
+    def price(self):
+        return sum( (i.price for i in self.items.all()) ) #TODO: add calculation of all price of order
+
+
+
+
+class Customer(User):
+    '''
+    User information model
+    '''
+    phone = models.CharField(verbose_name="phone number", max_length=20, null=True) #TODO: add phone validator validators=[]
+    addres = models.CharField(verbose_name="addres", max_length=255, null=True) #TODO: add adress validation
     
+    class Meta:
+        verbose_name = 'customer'
+        verbose_name_plural = 'customers'
