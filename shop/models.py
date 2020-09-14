3f98eda56b8942b64864d284a185ff0d5639a807
yarik2215 <stikblacklabel@gmail.com>
2020-09-14 20:59:47 +0300

    add STATUS field to Order model

--- shop/models.py
+++ shop/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
 
 
 class ItemType(models.Model):
     '''
     Item type model, store type of items that can be in shop.
     '''
-    name = models.CharField(verbose_name="name", max_length=100)
+    name = models.CharField(_("name"), max_length=100)
 
     def __str__(self):
         return self.name
@@ -23,13 +24,13 @@ class Item(models.Model):
     '''
     Shop item model.
     '''
-    name = models.CharField(verbose_name="name", max_length=100)
-    color_name = models.CharField(verbose_name="color name", max_length=50)
+    name = models.CharField(_("name"), max_length=100)
+    color_name = models.CharField(_("color name"), max_length=50)
     slug = models.SlugField(max_length=200, unique=True)
-    item_type = models.ForeignKey(ItemType, on_delete=models.SET_NULL, null=True, blank=True)
+    item_type = models.ForeignKey("ItemType", on_delete=models.SET_NULL, null=True, blank=True)
     image = models.ImageField(upload_to=upload_path)
     quantity = models.IntegerField()
-    price = models.DecimalField("price",max_digits=10,decimal_places=2)
+    price = models.DecimalField(_("price"),max_digits=10,decimal_places=2)
 
     def __str__(self):
         return self.name
@@ -52,10 +53,16 @@ class Order(models.Model):
     '''
     Order model, store information about orders.
     '''
+    class Status(models.IntegerChoices):
+        WAITING = 0, _("waiting")
+        PROCESSING = 1, _("processing")
+        DONE = 2, _("done")
+
     customer = models.ForeignKey('Customer', on_delete=models.CASCADE)  # add validation that user is in customer group
     items = models.ManyToManyField('Item',through='OrderItems')
-    order_date = models.DateField("order date")
+    order_date = models.DateField(_("order date"))
     delivery_address = models.CharField(max_length=255)
+    status = models.IntegerField(_("status"), choices=Status.choices, default=Status.WAITING)
 
     def price(self):
         return sum( (i.price for i in self.items.all()) ) #TODO: add calculation of all price of order
@@ -67,8 +74,8 @@ class Customer(User):
     '''
     User information model
     '''
-    phone = models.CharField(verbose_name="phone number", max_length=20, null=True) #TODO: add phone validator validators=[]
-    addres = models.CharField(verbose_name="addres", max_length=255, null=True) #TODO: add adress validation
+    phone = models.CharField(_("phone number"), max_length=20, null=True) #TODO: add phone validator validators=[]
+    addres = models.CharField(_("addres"), max_length=255, null=True) #TODO: add adress validation
     
     class Meta:
         verbose_name = 'customer'
